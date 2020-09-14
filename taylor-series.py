a7ff92fb58b261f5ce9255efc97fd14bb0cd4143
euneestella <eunee.stella@gmail.com>
2020-09-14 22:57:18 +0900

    taylor series expansion

---
+++ taylor-series.py
@@ -0,0 +1,21 @@
+import numpy as np
+import math
+
+def taylor(x, tol, m):
+    n = 1
+    y = x-1
+    summation = 0
+    power = y-1
+    term = y
+    sign = -1
+
+    while n <= m :
+        sign -= sign
+        summation = summation+sign*term
+        power = power*y
+        term = power/(n+1)
+        if abs(term)<tol:
+            return n
+        n+=1
+    
+    return n
