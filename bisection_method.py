f412ac6b80bc8f76090301840f0d9a0501b66d74
euneestella <eunee.stella@gmail.com>
2020-09-16 14:00:40 +0900

    bisection method

---
+++ bisection_method.py
@@ -0,0 +1,21 @@
+import numpy as np
+import math
+
+def f(x):
+    x**3+4*x**2-10
+
+def bisection(a, b, tol, maxiter):
+    FA = f(a)
+    i = 1
+    while i<=maxiter:
+        p=a+(b-a)/2
+        FP = f(p)
+        if FP == 0 or (b-a)/2 < tol:
+            return(p)
+        n += 1
+        if FA*FP>0:
+            a = p
+            FA = FP
+        else:
+            b=p
+    return("failure")
