9fe84856739ef36b9e9689820babd59a31388a36
euneestella <eunee.stella@gmail.com>
2020-09-22 01:57:27 +0900

    fixed-point iteration

---
+++ fixed-point-iteration.py
@@ -0,0 +1,15 @@
+import math
+
+def f(x):
+    return x-(x**3+4*x**2-10)/(3*x**2+8*x)
+
+def fixed_point(approx, tolerance, maxiter):
+    i = 1
+    while i <= maxiter:
+        fixedpt  = f(approx)
+        if abs(fixedpt - approx) < tolerance:
+            return (i, fixedpt)
+        else:
+            i+=1
+        approx = fixedpt
+        return ("unsuccessful")
