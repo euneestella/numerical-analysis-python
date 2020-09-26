abc1340c8382f86ce23d8994976f14b77cc41052
unknown <eunee.stella@gmail.com>
2020-09-26 21:01:28 +0900

    edited file name and variable

--- taylor_polynomial.py
+++ taylor_polynomial.py
@@ -1,20 +1,16 @@
-import math
-
 def taylor(x, tol, m):
     n = 1
     y = x-1
     summation = 0
-    power = y-1
+    power = y
     term = y
     sign = -1
-
     while n <= m :
         sign -= sign
         summation = summation+sign*term
         power = power*y
         term = power/(n+1)
         if abs(term)<tol:
-            return n
+            return n, summation
         n+=1
-    
-    return n
+    return n, summation
