6e284b61c7374805075cb494d804da0eae31ce09
unknown <eunee.stella@gmail.com>
2020-09-27 16:39:07 +0900

    secant method

---
+++ secant_method.py
@@ -0,0 +1,19 @@
+import math
+
+def f(x):
+    return math.cos(x)-x
+
+def secant(approx1, approx2, tol, maxiter):
+    i=2
+    approxval1 = f(approx1)
+    approxval2 = f(approx2)
+    while i <= maxiter:
+        approx = approx2 - approxval2*(approx2-approx1)/(approxval2 - approxval1)
+        if abs(approx-approx2) < tol:
+            return approx
+        i+=1
+        approx1 = approx2
+        approxval1 = approxval2
+        approx2 = approx
+        approxval2 = f(approx)
+    print("unsuccessful")
