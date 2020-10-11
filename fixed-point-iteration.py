7c21011a26ba15f34086d71d31e3f489ee83d557
unknown <eunee.stella@gmail.com>
2020-10-11 12:03:41 +0900

    변수 이름 통일

--- fixed-point-iteration.py
+++ fixed-point-iteration.py
@@ -1,13 +1,13 @@
 def f(x):
     return x-(x**3+4*x**2-10)/(3*x**2+8*x)
 
-def fixed_point(approx, tolerance, maxiter):
+def fixed_point(p, tol, maxiter):
     i = 1
     while i <= maxiter:
-        fixedpt  = f(approx)
-        if abs(fixedpt - approx) < tolerance:
+        fixedpt  = f(p)
+        if abs(fixedpt - p) < tol:
             return (i, fixedpt)
         else:
             i+=1
-        approx = fixedpt
+        p = fixedpt
     return ("unsuccessful")
