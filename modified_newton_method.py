eb3b1ae10f75f744c4369c428b47c22c828e67d1
unknown <eunee.stella@gmail.com>
2020-10-15 22:35:15 +0900

    목표 함수 수정

--- modified_newton_method.py
+++ modified_newton_method.py
@@ -1,13 +1,13 @@
 import math
 
 def f(x):
-    return math.exp(x)-x-1
+    return math.exp(6*x)+1.441*math.exp(2*x)-2.079*math.exp(4*x)-0.3330
 
 def g(x):
-    return math.exp(x)-1
+    return 6*math.exp(6*x)+2*1.441*math.exp(2*x)-4*2.079*math.exp(4*x)
 
 def h(x):
-    return math.exp(x)
+    return 6*6*math.exp(6*x)+2*2*1.441*math.exp(2*x)-4*4*2.079*math.exp(4*x)
 
 def modified_newton(approx1, tol, maxiter):
     i = 1
@@ -17,4 +17,4 @@ def modified_newton(approx1, tol, maxiter):
             return (approx, i)
         i += 1
         approx1 = approx
-    return (approx, i)
+    print("unsuccessful")
